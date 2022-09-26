632d69baf2c40635291d5e28e15c73d9ff2af8e3
PrasannaKumar999 <97685872+PrasannaKumar999@users.noreply.github.com>
2022-09-26 22:06:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,73 @@
-document.getElementById("box").style.display = "none";
-document.getElementById("addbox").style.display = "none";
- 
-    document.getElementById("righttext").onclick = function(){
-        document.querySelector(".blur").style.filter="blur(10px)";
-        document.getElementById("box").style.display = "block";
-        
+function clock(){
+    let time= new Date();
+    let hr=time.getHours();
+    let mi=time.getMinutes();
+    let se=time.getSeconds();
+    document.getElementById("hour").innerHTML = hr;
+    document.getElementById("min").innerHTML = mi;
+    document.getElementById("sec").innerHTML = se;
+    document.getElementById("ap").innerText = "AM"
+    if(hr>12){
+        hr = hr -12;
+     document.getElementById("hour").innerText = hr;
+     document.getElementById("ap").innerText = "PM"
+    } 
+    if(hr==0){
+        hr=12;
+     document.getElementById("hour").innerText = hr;
+     document.getElementById("ap").innerText = "PM"
     }
-    document.getElementById("closebtn").onclick = function(){
-        document.querySelector(".blur").style.filter="blur(0px)";
-        document.getElementById("box").style.display = "none";
+    if(hr<10)
+    document.getElementById("hour").innerText = "0"+hr;
+    if(mi<10)
+    document.getElementById("min").innerText = "0"+mi;
+    if(se<10)
+    document.getElementById("sec").innerText = "0"+se;
+}
+setInterval(clock,1)
+
+    document.getElementById("clk").onclick = function(){
+        var a=document.getElementById('Morning').value;
+            var b = document.getElementById("Afternoon").value;
+           var c = document.getElementById("Evening").value;
+           var d = document.getElementById("Night").value;
+           var hour = new Date().getHours();
+        if(a == hour){
+            document.getElementById("leftbox2").innerText="Good Morning!!! Wake Up !! "
+            document.getElementById("rightbox2").innerText="GRAB SOME HEALTHY BREAKFAST!!!"
+            document.getElementById('rightbox3').style.backgroundImage='url("./Images/Component 30 – 1.svg")';
+        }
+        else if(b == hour){          
+            document.getElementById('rightbox3').style.backgroundImage='url("./Images/Component 31 – 1.svg")';
+            document.getElementById("rightbox2").innerText="Let's Have Some Lunch !!";
+            document.getElementById("leftbox2").innerText="Good Afternoon !!";
+        }
+        else if(c == hour){
+            document.getElementById('rightbox3').style.backgroundImage='url("lunch_image.png")';
+            document.getElementById("rightbox2").innerText="STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!";
+            document.getElementById("leftbox2").innerText="Good Evening!!";
+        }
+        else if(d == hour){
+            document.getElementById('rightbox3').style.backgroundImage='url("./Images/Component 32 – 1.png")';
+            document.getElementById("rightbox2").innerText="CLOSE YOUR EYES AND GO TO SLEEP";
+            document.getElementById("leftbox2").innerText="Good Night!!";
+        } 
+        changetext()
     }
+    function changetext(){
+        let s = document.getElementById("Morning");
+        let displayMtext = s.options[s.selectedIndex].text;
+        document.getElementById("wakeup-time").innerText="Wake Up Time :"+ displayMtext;
 
-    document.getElementById("addbtn").onclick = function(){
-        document.querySelector(".blur").style.filter="blur(0px)";
-        document.getElementById("box").style.display = "none";
-        document.getElementById("itemstext").style.display = "none";
-            const totalbox = document.querySelector(".totalbox");
+        let f = document.getElementById("Afternoon");
+        let displayAtext = f.options[f.selectedIndex].text;
+        document.getElementById("lunch-time").innerText="Lunch Time :" +displayAtext ;
 
-            const newbox = document.createElement("div");
-            newbox.setAttribute("class","box") ;
+        let g = document.getElementById("Evening");
+        let displayEtext = g.options[g.selectedIndex].text;
+        document.getElementById("nap-time").innerText="Nap Time :"+displayEtext;
 
-            const heading = document.createElement("h2");
-            heading.setAttribute("class","box-heading");
-            heading.innerText = document.querySelector(".inputtext").value;
-
-            const addIcon = document.createElement("i");
-            addIcon.setAttribute("class","add-icon");
-
-            const deleteicon = document.createElement("span");
-            deleteicon.setAttribute("class","delete-icon");
-            
-            totalbox.appendChild(newbox);
-            newbox.appendChild(heading);
-            newbox.appendChild(addIcon);
-            newbox.appendChild(deleteicon);
-
-
-            addIcon.onclick = function(){
-                document.getElementById("addbox").style.display = "block";
-                document.querySelector(".blur").style.filter="blur(10px)";
-                const text = document.createElement("addtext");
-               text.setAttribute("class","addtext");
-            }
-            document.getElementById("closebtn2").onclick = function(){
-                document.querySelector(".blur").style.filter="blur(0px)";
-                document.getElementById("addbox").style.display = "none";
-            }
-
-            document.getElementById("addbtn2").onclick = function(){
-                document.getElementById("addbox").style.display="none";
-                document.querySelector(".blur").style.filter="blur(0px)";
-                let inputtext2 =document.querySelector(".inputtext2").value;
-                const box = document.querySelector(".box");
-               
-                let text = document.createElement("div");
-                text.setAttribute("class","box-text");
-                 text.innerHTML=inputtext2;
-                 box.appendChild(text);
-
-                let markdone = document.createElement("button");
-                markdone.setAttribute("class","markdone");
-                markdone.innerHTML="Mark Done";
-                text.appendChild(markdone);
-
-               document.querySelector(".markdone").onclick = function(){
-                    document.querySelector(".box-text").style.color = "red";
-                    document.querySelector(".box-text").style.textDecoration = "line-through";
-                    document.querySelector(".markdone").style.display = "none";
-                }
-            }
-            
-
+        let h = document.getElementById("Night");
+        let displayNtext = h.options[h.selectedIndex].text;
+        document.getElementById("night-time").innerText="Night Time :"+displayNtext;
     }
-
-
-         
-
-
-
